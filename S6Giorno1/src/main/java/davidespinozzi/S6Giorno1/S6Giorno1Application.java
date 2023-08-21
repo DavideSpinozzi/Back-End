f63a03d53dd18349287f8966621cf65ded8a5515
Davide Spinozzi <davidespinozzi01@gmail.com>
2023-08-21 22:16:47 +0200

    .

---
+++ S6Giorno1/src/main/java/davidespinozzi/S6Giorno1/S6Giorno1Application.java
@@ -0,0 +1,13 @@
+package davidespinozzi.S6Giorno1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6Giorno1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S6Giorno1Application.class, args);
+	}
+
+}
---
+++ S6Giorno1/src/test/java/davidespinozzi/S6Giorno1/S6Giorno1ApplicationTests.java
@@ -0,0 +1,13 @@
+package davidespinozzi.S6Giorno1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6Giorno1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
